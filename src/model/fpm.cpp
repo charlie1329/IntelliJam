c7968efaaafdf7de5f2b5983542e179cea863001
charlie1329 <charlie.street1@btinternet.com>
2018-03-07 12:50:22 +0000

    Up to predicting notes

--- src/model/fpm.cpp
+++ src/model/fpm.cpp
@@ -3,6 +3,11 @@
  * Author: Charlie Street
  */
 
+#include <utility>
+#include <cmath>
+#include <limits>
+#include <random>
+#include <chrono>
 #include "../../include/model/fpm.h"
 
 /**
@@ -13,7 +18,7 @@
  * @return the mapped point
  */
 VectorXd FPM::applyMap(VectorXd t_i, double k, VectorXd x) {
-    //TODO: Fill In!
+    return (k * x) + ((1.0 - k) * t_i);
 }
 
 
@@ -24,7 +29,17 @@ VectorXd FPM::applyMap(VectorXd t_i, double k, VectorXd x) {
   * @param S the sequence of notes
   */
 VectorXd FPM::toChaosRep(MatrixXd t, double k, vector<int> S) {
-    //TODO: Fill In!
+    int D = t.rows();
+
+    //initialise to centre of hypercube
+    VectorXd x = MatrixXd::Constant(D,1,0.5);
+
+    //now do the conversion to CBR
+    for(auto note : S) {
+       x = applyMap(t.col(note),k,x);
+    }
+
+    return x;
 }
 
 /**
@@ -34,7 +49,11 @@ VectorXd FPM::toChaosRep(MatrixXd t, double k, vector<int> S) {
  * @return the euclidean distance
  */
 double FPM::euclideanDistance(VectorXd x, VectorXd y) {
-    //TODO: Fill In!
+    double distance = 0.0;
+    for(int i = 0; i < x.rows(); i++) {
+        distance += pow((x(i,0)-y(i,0)),2);
+    }
+    return distance;
 }
 
 /**
@@ -46,7 +65,20 @@ double FPM::euclideanDistance(VectorXd x, VectorXd y) {
  * @return the index in B of the closest codebook vector
  */
 int FPM::findClosestCodebook(VectorXd x, MatrixXd B) {
-    //TODO: Fill In!
+    double minDistance = std::numeric_limits<double>::infinity(); //set to inifinitely high value
+    int minIndex = -1; //nothing found yet
+
+    //loop through the codebook vectors
+    for(int i = 0; i < B.cols(); i++) {
+        double newDistance = euclideanDistance(x,B.col(i));
+        if(newDistance < minDistance) {
+            minDistance = newDistance;
+            minIndex = i;
+        }
+    }
+
+    //return the index of the closest codebook vector
+    return minIndex;
 }
 
 /**
@@ -59,7 +91,27 @@ int FPM::findClosestCodebook(VectorXd x, MatrixXd B) {
  * @return the next note in the sequence
  */
 int FPM::predictNextNote(vector<int> s, MatrixXd B, MatrixXd N, MatrixXd t, double k) {
-    //TODO: Fill In!
+
+    //bring sequence to chaos representation
+    VectorXd x = toChaosRep(std::move(t),k,std::move(s));
+
+    //now find the closest codebook vector
+    int i = findClosestCodebook(x,std::move(B));
+    RowVectorXd distribution = N.row(i);
+    auto totalSamples = (int)distribution.sum();
+
+    //set up a random generator
+    default_random_engine gen;
+    gen.seed((unsigned int)std::chrono::system_clock::now().time_since_epoch().count());
+    uniform_int_distribution<int> rng(1,totalSamples);
+
+    //do some random sampling to generate the note
+    int randNo = rng(gen);
+    int total = 0;
+    for(int j = 0; j < distribution.cols(); j++) {
+        total += distribution(0,j);
+        if(randNo <= total) return j;
+    }
 }
 
 /**
@@ -104,7 +156,25 @@ MatrixXd FPM::readInMat(string filePath, int rows, int cols) {
  */
 FPM::FPM(string BNotePath, string NNotePath, string tNotePath, double kNote, double TNote,
         string BDirPath, string NDirPath, string tDirPath, double kDir, double TDir) {
-    //TODO: Fill In!
+
+    //read in note parameters
+    BNote = readInMat(std::move(BNotePath),4,6);
+    NNote = readInMat(std::move(NNotePath),6,13);
+    tNote = readInMat(std::move(tNotePath),4,13);
+    this->kNote = kNote;
+    this->TNote = TNote;
+
+    //read in direction parameters
+    BDir = readInMat(std::move(BDirPath),1,5);
+    NDir = readInMat(std::move(NDirPath),5,2);
+    tDir = readInMat(std::move(tDirPath),1,2);
+    this->kDir = kDir;
+    this->TDir = TDir;
+
+    //scale the NNote matrix by the temperature parameter
+    NNote = Eigen::pow(NNote.array(),1.0/TNote);
+
+
 }
 
 
@@ -114,7 +184,25 @@ FPM::FPM(string BNotePath, string NNotePath, string tNotePath, double kNote, dou
  * @param duration the duration of said note
  */
 void FPM::queueNote(int note, double duration) {
-    //TODO: Fill In!
+    absSequence.push_back(note); //just the note
+    if(note == 0) {
+        noteSequence.emplace_back(note,duration);
+        return; // nothing to add to direction sequence
+    } else {
+        noteSequence.emplace_back((note % 12) + 1,duration);
+    }
+
+    if(previousNote != 1 && previousNote != note) {
+        if(note > previousNote) {
+            dirSequence.push_back(1);
+        } else {
+            dirSequence.push_back(0);
+        }
+
+        previousNote = note;
+    }
+
+    if(previousNote == -1) previousNote = note;
 }
 
 /**
@@ -131,5 +219,10 @@ MatrixXd FPM::combinedPredict() {
  * member function wipes the arrays for the next round of prediction etc.
  */
 void FPM::clearState() {
-    //TODO: Fill In!
+    //clear all vectors
+    absSequence.clear();
+    noteSequence.clear();
+    dirSequence.clear();
+    //reinitialise
+    previousNote = -1;
 }
